69f3e1a8b1c563cbd955ee105a2e2065ad8c398f
fayez <fayezmohammed23@gmail.com>
2020-04-01 00:24:19 +0530

    teleram bot initial

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1107103850:AAGgmmYdIxWQTGwoa3rqm9q-mhR0nBVFhRo')
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+const bot = new Telegraf('');
+
+bot.start((ctx) => ctx.reply('Welcome!'));
+
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.launch();
