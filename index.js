016dc02c226f483c852f45a817dcef8c38aecb34
evolvingkd <48388666+rougerepublic23@users.noreply.github.com>
2020-04-01 00:25:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1107103850:AAGgmmYdIxWQTGwoa3rqm9q-mhR0nBVFhRo')
+const bot = new Telegraf('')
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
